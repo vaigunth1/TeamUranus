7c86cda18ef802bd15b5d3c6cc4a83af3fd5ceed
Vaigunth <vaigunthop@gmail.com>
2026-07-27 19:32:33 +0530

    Overwrite remote with current local code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/global.css";
+import "./Styles/global.css";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
